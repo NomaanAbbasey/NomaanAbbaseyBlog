456d7eae403a54c031eec41254a1e89a92a5832b
Nomaan <NomaanTheSupreme@bitbucket.org>
2019-04-30 20:34:12 -0400

    created models Post and Author

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+# Author model represents the author data
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
+
+# Post Model represents the information about the post
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1600)
+    pub_date = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.title
+
+
